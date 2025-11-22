9b1d38f7a276635502c049b4632319a4e371eb33
Heesu Chung <caabsu123@gmail.com>
2025-11-21 19:17:42 -0800

    Fix profile badge placement closing tag

--- src/app/image/page.tsx
+++ src/app/image/page.tsx
@@ -1325,7 +1325,6 @@ export default function ImageStudioPage() {
           {saveToast.message}
         </div>
       )}
-    </div>
       <ProfileBadge
         activeProfile={activeProfile}
         onSelect={(id) => setActiveProfileId(id)}
